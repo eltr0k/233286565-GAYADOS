35652e0b346be8cefd06899aa793191c4fc8215a
eltr0k <ddg6565@students.uc-bcf.edu.ph>
2025-03-06 15:50:18 +0800

    exercise1

---
+++ PHP Exercises/exercise1.php
@@ -0,0 +1,16 @@
+<?php
+$a = 15;
+$b = 7;
+
+$sum = $a + $b;
+$difference = $a - $b;
+$product = $a * $b;
+$quotient = $a / $b;
+
+echo "Variable a: " . $a . "\n";
+echo "Variable b: " . $b . "\n";
+echo "Sum: " . $sum . "\n";
+echo "Difference: " . $difference . "\n";
+echo "Product: " . $product . "\n";
+echo "Quotient: " . $quotient . "\n";
+?>
